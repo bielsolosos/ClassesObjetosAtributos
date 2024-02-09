1f97b4d094cb6e3e1b37de93a81e2240b6f96b5d
Gabriel Coutinho <125703161+bielsolosos@users.noreply.github.com>
2024-02-08 21:11:34 -0300

    Só pra eu Salvar dps (espero não esquecer de apagar)

---
+++ Exercício de Fixação 4/Exercício de Fixação 4/ConversorDeMoeda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_de_Fixação_4
+{
+    internal class ConversorDeMoeda
+    {
+        public static double IOF = 6.0;
+
+        public static double Converter_Dolar(double R, double cotacao)
+        {
+            double dolar = R * cotacao;
+            return dolar + ((dolar * IOF) / 100);
+        }
+    }
+}
---
+++ Exercício de Fixação 4/Exercício de Fixação 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using Exercício_de_Fixação_4;
+using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Cotacao, Dolares;
+            Console.Write("Qual a cotação do dolar?");
+            Cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dolares você vai comprar camarada?");
+            Dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Valor a ser pago em Reais {ConversorDeMoeda.Converter_Dolar(Dolares, Cotacao).ToString("F2")}");
+        }
+    }
+}

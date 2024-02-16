762f73f02594f95242474a020298da3bd99388c8
Gabriel Coutinho <125703161+bielsolosos@users.noreply.github.com>
2024-02-16 13:19:45 -0300

    Versão Alterando a Classe Principal do Exercício

---
+++ Exercício Fixação 5/Exercício Fixação 5/Fichas_Banco.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_Fixação_5
+{
+    internal class Fichas_Banco
+    {
+        public double Saldo_Banco {  get; private set; }
+        public int Numero_Conta { get; private set; }
+        public String Nome_Titular { get; set; }
+
+        public Fichas_Banco(double Saldo, int Numero,String Nome)
+        {
+            Saldo_Banco = Saldo;
+            Numero_Conta = Numero;
+            Nome_Titular = Nome;
+        }
+
+        public Fichas_Banco(int Numero, String Nome)
+        {
+            Saldo_Banco = 0;
+            Numero_Conta = Numero;
+            Nome_Titular = Nome;
+        }
+
+        public void AdicionaSaldo(double Valor)
+        {
+            Saldo_Banco += Valor;
+        }
+
+        public void RemoverSaldo(double Valor)
+        {
+            Saldo_Banco -= Valor;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome_Titular + ", Número da Conta: " + Numero_Conta + ", Saldo: $ " + Saldo_Banco ;
+        }
+    }
+}
---
+++ Exercício Fixação 5/Exercício Fixação 5/Program.cs
@@ -0,0 +1,23 @@
+﻿using Exercício_Fixação_5;
+using System;
+
+namespace Exerc5 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double Valor_Adicionado = 222.00;
+            Fichas_Banco Conta = new Fichas_Banco(2003, "Gabriel");
+            Console.WriteLine(Conta);
+            Console.WriteLine($"\nSaldo Adicionado: {Valor_Adicionado} ");
+            Conta.AdicionaSaldo(Valor_Adicionado);
+            Console.WriteLine(Conta);
+            Console.WriteLine($"\nSaldo Removido: {Valor_Adicionado} ");
+            Conta.RemoverSaldo(Valor_Adicionado);
+            Console.WriteLine(Conta);
+
+
+        }
+    }
+}
